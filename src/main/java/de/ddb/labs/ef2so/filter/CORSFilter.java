9aa1d11e2cb86164e82431a19c0efbf83796c74d
Michael Büchner <m.buechner@dnb.de>
2021-04-21 16:29:53 +0200

    Added back CORS support

---
+++ src/main/java/de/ddb/labs/ef2so/filter/CORSFilter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018-2021 Michael Büchner, Deutsche Digitale Bibliothek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.ddb.labs.ef2so.filter;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Michael Büchner <m.buechner@dnb.de>
+ */
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    /**
+     * Adds a CORS header the all responses
+     *
+     * @param request HTTP Request
+     * @param response
+     * @throws java.io.IOException
+     */
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        
+        String allowOrigin = "*";
+        if (request.getHeaderString("Origin") != null && !request.getHeaderString("Origin").isEmpty()) {
+            allowOrigin = request.getHeaderString("Origin");
+        }
+
+        response.getHeaders().add("Access-Control-Allow-Origin", allowOrigin);
+        response.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+        response.getHeaders().add("Access-Control-Allow-Methods", "GET");
+    }
+}
